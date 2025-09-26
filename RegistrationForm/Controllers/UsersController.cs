94e1e648fab14114e5e3179b24d39fb01fb9cf95
Mahla <Mahla@DESKTOP-F7B9RSJ>
2025-09-26 13:39:08 +0330

    Add project files.

---
+++ RegistrationForm/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationForm.Models;
+
+namespace RegistrationForm.Controllers
+{
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            ViewData["Message"] = "سلام به سایت مهلا خوش آمدین";
+            ViewBag.CurrentTime=DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss");
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Result()
+        {
+         
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Users user)
+        {
+            if (ModelState.IsValid)
+            {
+                TempData["massage"] = "ثبت نام موفقیت‌آمیز بود!";
+                return  RedirectToAction("Result");
+            }
+            else
+            {
+                TempData["massage"] = "لطفا اطلاعات را کامل وارد نمایید!";
+                return RedirectToAction("Result");
+            }
+      
+        }
+    }
+}
---
+++ RegistrationForm/Models/Users.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationForm.Models
+{
+    public class Users
+    {
+        [Required(ErrorMessage = "وارد کردن نام الزامی است")]
+        public string Name {  get; set; }
+
+        [Required(ErrorMessage = "وارد کردن ایمیل الزامی است")]
+        [EmailAddress(ErrorMessage = "ایمیل نامعتبر است.")]
+        public string Email {  get; set; }
+
+        public DateTime CurrentTime { get; set; }   
+
+    }
+}
